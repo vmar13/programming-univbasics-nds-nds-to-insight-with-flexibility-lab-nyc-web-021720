2eed1d0092ae4f0ed9f88b046cff3079661b84cd
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 05:12:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -64,6 +64,8 @@ end
 
 
 def gross_per_studio(collection)
+  binding.pry
+  
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
